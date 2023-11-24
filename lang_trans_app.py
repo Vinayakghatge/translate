a6eddf2ab24f0b060073225deabe46fa9151d229
Vinayakghatge <151889344+Vinayakghatge@users.noreply.github.com>
2023-11-24 11:36:20 +0530

    Update lang_trans_app.py

--- lang_trans_app.py
+++ lang_trans_app.py
@@ -20,7 +20,7 @@ text_inp = st.text_input("InputText:", default_text)
 API_KEY = st.text_input("Enter your HuggingFace API key", type="password")
 
 # HuggingFace API inference URL.
-API_URL = "https://api-inference.huggingface.co/models/Helsinki-NLP/opus-mt-ko-en"
+API_URL = "https://api-inference.huggingface.co/models/Helsinki-NLP/pus-mt-en-ru"
 
 headers = {"Authorization": f"Bearer {API_KEY}"}
 
